88da6386282652546b553e319b3de369f49922ab
siege2000 <90224251+siege2000@users.noreply.github.com>
2024-03-25 09:51:49 +1300

    search stock by PLU or Tradename complete.

--- mssql-server/controllers/stockController.js
+++ mssql-server/controllers/stockController.js
@@ -17,6 +17,45 @@ const getStock = async (res) => {
   }
 };
 
+const searchStock = async ({ plu, tradeName }) => {
+  const query = {
+    where: {},
+  };
+
+  // Construct query conditions based on provided search parameters
+  const searchConditions = [];
+
+  if (plu) {
+    searchConditions.push({ PLU: plu });
+  }
+
+  if (tradeName) {
+    searchConditions.push({
+      TradeName: {
+        [Op.like]: `${tradeName}%`,
+      },
+    });
+  }
+
+  if (searchConditions.length) {
+    query.where = {
+      [Op.or]: searchConditions,
+      [Op.and]: { StockType: 0 },
+    };
+  }
+  query.attributes = [
+    "StockID",
+    "PLU",
+    "TradeName",
+    "PLU",
+    "RealCost",
+    "Retail",
+  ];
+
+  return Models.Stock.findAll(query);
+};
+
 module.exports = {
   getStock,
+  searchStock,
 };
