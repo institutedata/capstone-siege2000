f3cd1f4db025ea670f75a2d191cc99784c1aa6b6
siege2000 <90224251+siege2000@users.noreply.github.com>
2024-03-26 21:06:51 +1300

    broken again

--- client/src/components/stockSalesChart.jsx
+++ client/src/components/stockSalesChart.jsx
@@ -1,72 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState, useMemo } from "react";
+import axios from "axios";
 
-import {Typography,  } from '@mui/material';
+import { Typography } from "@mui/material";
 
-import moment from 'moment';
+import moment from "moment";
 
-import { BarChart } from '@mui/x-charts/BarChart';
-
-
-
+import { BarChart } from "@mui/x-charts/BarChart";
 
 const StockSalesChart = (props) => {
   const stockID = props.stockID;
-  console.log(stockID)
-    const [historyResults, setHistoryResults] = useState([]);
+
+  console.log("stockId", stockID);
+
+  const [historyResults, setHistoryResults] = useState([]);
+  const [months, setMonths] = useState([]);
+  const [qtySoldData, setQtySoldData] = useState([]);
+
+  useEffect(() => {
     const apiUrl = `http://localhost:8080/api/history/search?stockID=${stockID}`;
-    console.log(apiUrl);
-
-
-    useEffect(() => {
-      
-      const getHistoryDetails = async () => {
-        try {
-          console.log("History request received");
-          const response = await axios.get(apiUrl);
-          console.log(` Data Response ${response.data}`);
-          setHistoryResults(response.data); // API Data the array of history
-          
-        }catch(error){
-          console.error("There was a problem with your fetch operation:", error);
-        }
-
+    console.log("apiUrl", apiUrl);
+    const fetchStockDetails = async () => {
+      try {
+        console.log("History request received");
+        const response = await axios.get(apiUrl);
+        console.log("I am in USE EFFECT: ", response?.data);
+        setHistoryResults(response?.data); // API Data the array of history
+        setMonths(response.data.map((row) => row.Month));
+        setQtySoldData(response.data?.map((row) => row.QtySold));
+      } catch (error) {
+        console.error("There was a problem with your fetch operation:", error);
+      }
     };
-    getHistoryDetails()
-    
-  },[stockID]
-    )
-    console.log(`HistoryResults2 ${historyResults }`)
-    const months = historyResults.map((row) => (row.Month))
-    const qtySoldData = historyResults.map((row) => row.QtySold)
+    fetchStockDetails();
+  }, [stockID]);
 
-    console.log(months) 
-    
+  // useEffect(() => {
+  //   console.log(`HistoryResults ${historyResults}`);
+  // }, [historyResults])
 
+  // console.log("Months: ", months);
 
-    
-  
-    
-    
-    return    (
-      
-      <React.Fragment>
-        <Typography variant="h6">Stock Sales Chart for {stockID}</Typography>
-        
-        
-          
+  return (
+    <React.Fragment>
+      <Typography variant="h6">Stock Sales Chart for {stockID}</Typography>
+
       <BarChart
-      series={[
-        { data:qtySoldData}        
-        
-      ]}
-      height={290}
-      xAxis={[{ data: months, scaleType: 'band' }]}
-      margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-    />
- 
+        series={[{ data: qtySoldData }]}
+        height={290}
+        xAxis={[{ data: months, scaleType: "band" }]}
+        margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+      />
     </React.Fragment>
-    
-    )
-    }
+  );
+};
 export default StockSalesChart;
--- client/src/pages/StockDetails.jsx
+++ client/src/pages/StockDetails.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import { FormControl, Stack, TextField, Typography } from '@mui/material';
-import StockSalesChart from '../components/stockSalesChart';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { FormControl, Stack, TextField, Typography, Box } from "@mui/material";
+import StockSalesChart from "../components/stockSalesChart";
 
 const StockDetails = () => {
   const { stockID } = useParams();
@@ -22,42 +22,63 @@ const StockDetails = () => {
 
     fetchStockDetails();
   }, [stockID, apiUrl]);
+  
 
   return (
     <div className="posts">
-      <Typography variant="h5">Stock Details for Stock ID: {stockID}</Typography>
-      
+      <Typography variant="h5">
+        Stock Details for Stock ID: {stockID}
+      </Typography>
+
       <Typography variant="h6">Stock Details</Typography>
       <form>
-       
-                {searchResults.map((stock) => (
-                    <div key={stock.StockID}>
-                        <Typography>PLU: {stock.PLU}</Typography>
-                        <Typography>Trade Name: {stock.TradeName}</Typography>
-                        <Typography>Real Cost: {stock.RealCost}</Typography>
-                        <Typography>Retail: {stock.Retail}</Typography>
-                    <Stack spacing="2">
-                    <FormControl  sx={{flexDirection:'row', m:4, p:2, border:1}} >
-                        
-                        <TextField sx={{m:0}}label="PLU/Pharmacode" defaultValue={stock.PLU} />
-                        <TextField sx={{ml:1, width:'auto', minWidth:'480px'}}label="Trade Name" defaultValue={stock.TradeName} />
-                      </FormControl>
-                      <FormControl  sx={{flexDirection:'row', m:4, border:1, mt:2, p:1}} >
-                        <Typography variant="h10"sx={{m:0}}>Stock Details</Typography>
-                        <TextField sx={{m:0, mt:2}}label="Real Cost" defaultValue={stock.RealCost} />
-                        <TextField sx={{ml:1, mt:2}}label="Retail" defaultValue={stock.Retail} />
-                        <TextField sx={{ml:1, mt:2}}label="SOH" defaultValue={stock.SOH} />
-                        
-                    </FormControl>
-                    <FormControl  sx={{flexDirection:'row', m:4, border:1, mt:2, p:1}} >
-                       <StockSalesChart stockID = {stockID}/>
-
-                        </FormControl>
-                    </Stack>
-                    </div>
-                ))}
-            
-           
+        {searchResults.map((stock) => (
+          <div key={stock.StockID}>
+            <Typography>PLU: {stock.PLU}</Typography>
+            <Typography>Trade Name: {stock.TradeName}</Typography>
+            <Typography>Real Cost: {stock.RealCost}</Typography>
+            <Typography>Retail: {stock.Retail}</Typography>
+            <Stack spacing="2">
+              <FormControl sx={{ flexDirection: "row", m: 4, p: 2, border: 1 }}>
+                <TextField
+                  sx={{ m: 0 }}
+                  label="PLU/Pharmacode"
+                  defaultValue={stock.PLU}
+                />
+                <TextField
+                  sx={{ ml: 1, width: "auto", minWidth: "480px" }}
+                  label="Trade Name"
+                  defaultValue={stock.TradeName}
+                />
+              </FormControl>
+              <FormControl
+                sx={{ flexDirection: "row", m: 4, border: 1, mt: 2, p: 1 }}
+              >
+                <Typography variant="h10" sx={{ m: 0 }}>
+                  Stock Details
+                </Typography>
+                <TextField
+                  sx={{ m: 0, mt: 2 }}
+                  label="Real Cost"
+                  defaultValue={stock.RealCost}
+                />
+                <TextField
+                  sx={{ ml: 1, mt: 2 }}
+                  label="Retail"
+                  defaultValue={stock.Retail}
+                />
+                <TextField
+                  sx={{ ml: 1, mt: 2 }}
+                  label="SOH"
+                  defaultValue={stock.SOH}
+                />
+              </FormControl>
+              <Box sx={{ flexDirection: "row", m: 4, border: 1, mt: 2, p: 1 }}>
+                <StockSalesChart stockID={stockID} />
+              </Box>
+            </Stack>
+          </div>
+        ))}
       </form>
     </div>
   );
