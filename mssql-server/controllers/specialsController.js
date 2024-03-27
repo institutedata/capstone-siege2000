3e32c186b8fbcb566ba4eecc889884c8e7bef81a
siege2000 <90224251+siege2000@users.noreply.github.com>
2024-03-27 14:03:11 +1300

    updated backen for specials so now brings across stock, special items and specials data.

--- mssql-server/controllers/specialsController.js
+++ mssql-server/controllers/specialsController.js
@@ -1,6 +1,7 @@
 "use strict";
 const { Op } = require("sequelize");
 const Models = require("../models/init-models");
+const sequelize = require("sequelize");
 
 const getSpecials = async (res) => {
   // Old Code - delete when finished testing new code.
@@ -16,9 +17,38 @@ const getSpecials = async (res) => {
   // } catch (error) {
   //   res.status(500).json({ error: error.message });
   // }
+
+  try {
+    //const specialId = req.params.specialId;
+    const specials = await Models.SpecialItems.findAll({
+      include: [
+        {
+          model: Models.Stock,
+          required: true,
+          attributes: ["plu", "TradeName", "Retail", "RealCost"],
+        },
+        {
+          model: Models.Specials,
+          required: true,
+          where: { ActiveSpecial: 1 },
+          attributes: ["SpecialID", "FinishDate", "SpecialName"],
+        },
+      ],
+      where: { siDeleted: 0 },
+      attributes: [
+        ["SpecialRetail", "SpecialPrice"],
+        "Discount",
+        "MultiBuyText",
+        ["SPECIALITEMSDateModified", "DateModified"],
+      ],
+      order: [[sequelize.col("SPECIALITEMSDateModified"), "ASC"]],
+    });
+    res.json(specials);
+  } catch (error) {
+    console.error("Error fetching specials:", error);
+    res.status(500).send("Server error");
+  }
 };
-
-
 
 module.exports = {
   getSpecials,
