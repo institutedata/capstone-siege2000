7e7845bf5817414576b448c851a1008061b54a5b
siege2000 <90224251+siege2000@users.noreply.github.com>
2024-03-29 13:23:51 +1300

    Add cors dependency and update server port. Added link in the posts table model. tested model in Bruno.

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,47 +1,50 @@
-const {DataTypes, Model} = require('sequelize');
-let dbConnect = require('../config/dbConnect');
+const { DataTypes, Model } = require("sequelize");
+let dbConnect = require("../config/dbConnect");
 const sequelizeInstance = dbConnect.Sequelize;
 class Post extends Model {}
 
 Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        required: true,
-        defaultValue: DataTypes.NOW,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        required: true,
-      },
-      content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        required: true,
-      },
-      image: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        required: true,
-    
-      },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-      sequelize: sequelizeInstance,
-      modelName: "posts",
-      // uselowercase plural format
-      timestamps: true,
-      freezeTableName: true,
-    }
-  );
-  
-  module.exports = Post;
-  
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      required: true,
+      defaultValue: DataTypes.NOW,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      required: true,
+    },
+    link: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      required: true,
+    },
+  },
+  {
+    sequelize: sequelizeInstance,
+    modelName: "posts",
+    // uselowercase plural format
+    timestamps: true,
+    freezeTableName: true,
+  }
+);
+
+module.exports = Post;
